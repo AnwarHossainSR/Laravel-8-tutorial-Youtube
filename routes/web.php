25f93a3be65c9a16d9d81f11edceda84c86e441c
AnwarHossainSR <mahedianwar@gmail.com>
2021-04-16 16:35:25 +0600

    Model binding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class,'DatabaseConnection']);
+Route::get('/', [TeacherController::class,'fetchData']);
+Route::get('/students', [StudentController::class,'fetchStudents']);
