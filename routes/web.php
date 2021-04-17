15bea942732bd5a777304adcb492ccbfcf5fd0fb
AnwarHossainSR <mahedianwar@gmail.com>
2021-04-17 12:10:33 +0600

    Request class

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UriController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [StudentController::class,'fetchStudents']);
+Route::get('/', [UriController::class,'retriveUri']);
+Route::post('/user/register', [UriController::class,'showData']);
