21bf7a15c46db1747e3c66afff62bad1b278563a
AnwarHossainSR <mahedianwar@gmail.com>
2021-04-18 21:19:09 +0600

    Data fetch from database

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    public function index()
+    {
+        $students = Student::all();
+        return \view('studentList',\compact('students'));
+    }
     
     public function Create(Request $req)
     {
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('student');
-});
+Route::get('/', [StudentController::class,'index']);
 
 Route::post('/store', [StudentController::class,'Create']);
